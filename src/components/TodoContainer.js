1b40c5e07c6d3290bcbcd6d404da1ab2f7a6cacf
Alick Nyirenda <44676055+Beardless-sheik@users.noreply.github.com>
2022-02-08 13:48:41 +0200

    Save toDos in local storage to preserve data after refresh

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -6,25 +6,26 @@ import InputTodo from "./InputTodo";
 
 class TodoContainer extends React.Component {
   state = {
-    todos: [
-      {
-        id: uuidv4(),
-        title: "Setup development environment",
-        completed: true
-      },
-      {
-        id: uuidv4(),
-        title: "Develop website and add content",
-        completed: true
-      },
-      {
-        id: uuidv4(),
-        title: "Deploy to live server",
-        completed: false
-      }
-    ]
-   };
+    todos: [],
+  };
 
+  componentDidMount() {
+    const temp = localStorage.getItem("todos")
+    const loadedTodos = JSON.parse(temp)
+    if (loadedTodos) {
+      this.setState({
+        todos: loadedTodos
+      })
+    }
+  } 
+
+  componentDidUpdate(prevProps, prevState) {
+    if(prevState.todos !== this.state.todos) {
+      const temp = JSON.stringify(this.state.todos)
+      localStorage.setItem("todos", temp)
+    }
+  }
+  
   handleChange = (id) => {
     this.setState(prevState => ({
       todos: prevState.todos.map(todo => {
